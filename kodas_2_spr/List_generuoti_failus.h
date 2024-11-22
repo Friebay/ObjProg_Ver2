d8f400e1d0738453cc4dc1877c33b4ad420feee2
Friebay <frybaylt@gmail.com>
2024-11-22 08:54:37 +0200

    Nereikalingų sprendimų ištrynimas

--- kodas_2_spr/List_generuoti_failus.h
+++
@@ -1,9 +0,0 @@
-#ifndef LIST_GENERUOTI_FAILUS_H
-#define LIST_GENERUOTI_FAILUS_H
-
-#include "List_Biblioteka.h"
-
-void List_generuotiStudentuFaila(int studentuKiekis, const string& failoPavadinimas);
-void List_generuotiFaila();
-
-#endif
